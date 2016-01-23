776d197d24c1de94b393f083aee8dfcaa1a1d7ef
Elisa Huang <tingyih@uci.edu>
2016-01-22 16:38:38 -0800

    changed folder name to mips processor; change rom

--- mips processor/rom.vhd
+++ mips processor/rom.vhd
@@ -2,15 +2,15 @@ library IEEE;
 use IEEE.STD_LOGIC_1164.all; 
 use STD.TEXTIO.all;
 use IEEE.NUMERIC_STD_UNSIGNED.all;
-entity imem is -- instruction memory
+entity rom is -- instruction memory
 port(
 	addr: IN STD_LOGIC_VECTOR(31 downto 0); 
 --template-> a: in STD_LOGIC_VECTOR(5 downto 0);
 --     	    rd: out STD_LOGIC_VECTOR(31 downto 0)
 	dataIO: INOUT STD_LOGIC_VECTOR(31 DOWNTO 0)
 );
-end imem;
-architecture behavior of imem is
+end rom;
+architecture behavior of rom is
 begin
 process is
 file mem_file: TEXT;
