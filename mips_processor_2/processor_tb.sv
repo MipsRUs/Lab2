3492248e37b670325df3e4a30a28698624f6b7fb
Stefan Cao <caostefan@gmail.com>
2016-01-23 19:58:47 -0800

    debuggin timing

--- mips_processor_2/processor_tb.sv
+++ mips_processor_2/processor_tb.sv
@@ -13,13 +13,71 @@ processor L1(
 //end
 
 initial begin
-	ref_clk = 1'b0;
-	forever #50 ref_clk = ~ref_clk;
-	
 
+
+	//ref_clk = 1'b0;
+	//forever #50 ref_clk = ~ref_clk;
+
+
+	//reset = 1;
+	//#100;
+	//reset = 0;
+
+	ref_clk = 0;
 	reset = 1;
-	#100;
+	#10;
+	ref_clk = 1;
+	#10;
+	ref_clk = 0;
+	#10;
+	ref_clk = 1;
+	#10
+	ref_clk = 0;
 	reset = 0;
+	#10;
+	ref_clk = 1;
+	#10;
+	ref_clk = 0;
+	#10;
+	ref_clk = 1;
+	#10;
+	ref_clk = 0;
+	#10;
+	ref_clk = 1;
+	#10;
+	ref_clk = 0;
+	#10;
+	ref_clk = 1;
+	#10;
+	ref_clk = 0;
+	#10;
+	ref_clk = 1;
+	#10;
+	ref_clk = 0;
+	#10;
+	ref_clk = 1;
+	#10;
+	ref_clk = 0;
+	#10;
+	ref_clk = 1;
+	#10;
+	ref_clk = 0;
+	#10;
+	ref_clk = 1;
+	#10;
+	ref_clk = 0;
+	#10;
+	ref_clk = 1;
+	#10;
+	ref_clk = 0;
+	#10;
+
+
+
+
+
+
+
 
 end
 endmodule
